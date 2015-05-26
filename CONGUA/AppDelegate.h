e4c92ef7629e671089ef2012e3cb30a18b29a538
Soumen <admin@Soumens.local>
2015-05-26 14:59:25 +0530

    Initial Commit

---
+++ CONGUA/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CONGUA
+//
+//  Created by Soumen on 26/05/15.
+//  Copyright (c) 2015 Sandeep Dutta. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CONGUA/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CONGUA
+//
+//  Created by Soumen on 26/05/15.
+//  Copyright (c) 2015 Sandeep Dutta. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
